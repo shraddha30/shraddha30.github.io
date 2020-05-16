248ee964206cddfbb41c48c79bf4881e47499474
shraddha30 <shraddha.bagrodia@gmail.com>
2020-05-15 17:53:15 -0700

    add title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Header from './header';
+import { Helmet } from "react-helmet"
 
 import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>A11ys - an Accessibility blog</title>
+      </Helmet>
       <Header />
       <main>{children}</main>
       <footer>
